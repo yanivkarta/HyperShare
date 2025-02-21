347f35e0a4031f6b4019f5ec66fe919bdc7f3a6c
= <=>
2025-02-21 07:32:08 -0800

    check for cpu_features

--- isomatrixpython/tests/run_tests.cpp
+++ isomatrixpython/tests/run_tests.cpp
@@ -7,13 +7,19 @@
 #include "test1.h"
 #include "test2.h"
 
+//for builtin AVX512
+#include <immintrin.h>
 
 std::atomic_uint64_t provallo::tag_hyperplane::hplane_count(0); 
 
 
-
+//turn on AVX512:
+//
+//declare naked for AVX512
+//
+ 
 int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
+     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }   
 
